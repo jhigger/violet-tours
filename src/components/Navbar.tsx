35cb9b38fe0806d54c41514b18858a500058d737
jhigger <njohnjhigger@gmail.com>
2024-03-31 14:39:17 +0800

    add mobile sidebar, edit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,14 @@
+import { Menu } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 
 const links = [
   {
@@ -28,16 +37,45 @@ const NavLink = ({ name, href }: { name: string; href: string }) => {
   );
 };
 
+const Sidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Menu</SheetTitle>
+        </SheetHeader>
+        <div className="flex w-full flex-col gap-4 pt-8">
+          {links.map((link) => (
+            <SheetClose key={link.name} asChild>
+              <NavLink name={link.name} href={link.href} />
+            </SheetClose>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
 const Navbar = () => {
   return (
     <section className="bg-white">
       <div className="container mx-auto flex flex-col flex-wrap items-center justify-between py-5 md:flex-row">
-        <Link href="/" className="text-xl font-black leading-none">
-          <span className="text-primary">VioletTours</span>
-          <span className="text-indigo-600">.</span>
-        </Link>
-        <div className="flex flex-col divide-x-0 divide-y md:flex-row md:divide-x md:divide-y-0">
-          <nav className="flex flex-wrap items-center justify-center gap-3">
+        <div className="flex w-full items-center justify-between md:w-max">
+          <Link href="/" className="text-xl font-black leading-none">
+            <span className="text-primary">VioletTours</span>
+            <span className="text-indigo-600">.</span>
+          </Link>
+          <div className="md:hidden">
+            <Sidebar />
+          </div>
+        </div>
+        <div className="flex w-full flex-col divide-x-0 divide-y md:w-max md:flex-row md:divide-x md:divide-y-0">
+          <nav className="hidden flex-wrap items-center justify-center gap-3 md:flex">
             {links.map((link) => (
               <NavLink key={link.name} name={link.name} href={link.href} />
             ))}
